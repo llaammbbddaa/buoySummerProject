64aacf776b792111785c41a05ac3800fa1f912f7
Arshile Gorky <sarroufjohn@gmail.com>
2025-07-15 14:53:53 -0700

    Create protoIndoEuropean.ino

---
+++ protoIndoEuropean.ino
@@ -0,0 +1,72 @@
+// protoIndoEuropean (idk, i think its a funny name, and i like linguistics)
+// july fifteenth, johnny sarrouf
+// ideally, the prototype for the data collection for the buoy
+// github, https://github.com/llaammbbddaa/buoySummerProject/
+
+// components
+// geniuno mega
+// temp sensor, digi 52
+// i2c oxy sensor, scl1 & sda1, 0x73 address
+// micro sd card read / write, 53-50
+
+// all the libraries
+#include <Wire.h>
+#include <DS18B20.h>
+#include <DFRobot_OxygenSensor.h>
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <SD.h>
+
+// various init stuff
+DS18B20 tempSensor(48);
+DFRobot_OxygenSensor oxySensor;
+File dataWrite;
+
+void setup() {
+
+  Serial.begin(9600);
+  Wire.begin();
+
+  if (!rtc.begin()) {
+    Serial.println("RTC not found");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("Setting RTC time...");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));  // sets to compile time
+  }
+
+  if (!oxySensor.begin(0x73)) {
+    Serial.println("oxy sensor dne");
+    while (1);
+  }
+
+  if (!SD.begin(53)) {
+    Serial.println("initialization failed!sfidsajfoj");
+    while (1);
+  }
+
+}
+
+void fileWrite(String data) {
+  String fileName = ("dataBuoy.txt" + DateTime.now());
+  dataWrite = SD.open(fileName, FILE_WRITE);
+  if (dataWrite) {
+    dataWrite.println(data);
+    dataWrite.close();
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int temp = tempSensor.getTempF();
+  // the ten is there because it takes the averages of the number that you give, the higher the more smooth the value
+  float oxy = oxySensor.getOxygenData(10);
+  Serial.println("newData");
+  Serial.println(temp);
+  Serial.println(oxy);
+
+  delay(750);
+}
